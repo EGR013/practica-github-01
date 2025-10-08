a84968d1b088df68fa6ac6d7c1a9c8138a01911c
EGR013 <erik.garcia1@campus.xaloc.org>
2025-10-08 09:50:00 +0200

    Update ej-condicional21.py

--- ejercicios condicionales/ej-condicional21.py
+++ ejercicios condicionales/ej-condicional21.py
@@ -1,13 +1,13 @@
 #21. Programa que calcula una ecuación de segundo grado. Controla que el valor de la raíz cuadrada no de un valor negativo
 import math
-Ax=int(input("introduce el numero a "))
-Bx=int(input("introduce el numero b "))
-Cx=int(input("introduce el numero c "))
+Ax=float(input("introduce el numero a "))
+Bx=float(input("introduce el numero b "))
+Cx=float(input("introduce el numero c "))
 X=(Bx**2)-(4*Bx*Cx)
 Raiz=math.sqrt(X)
-if Raiz<0:
+if X<0:
     print("la raiz no puede ser negativa")
-x1=(-Bx+Raiz)/2*Ax
-x2=(-Bx-Raiz)/2*Ax
+x1=(-1*Bx)+Raiz/(2*Ax)
+x2=(-1*Bx)-Raiz/(2*Ax)
 print("el valor de x1 es ",x1)
 print("el valor de x2 es ",x2)
