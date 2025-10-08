57e5616472d838f42f0eced50f0e7501c5c6226f
EGR013 <erik.garcia1@campus.xaloc.org>
2025-10-08 09:38:29 +0200

    ej 2 finales "i'm freee" says person currently in prison
    hey los acabado , Yaay!

--- ejercicios condicionales/ej-condicionales34.py
+++ ejercicios condicionales/ej-condicionales34.py
@@ -1,11 +1,14 @@
 #34. Corrige los 4 errores o añade el código que necesites para que el siguiente programa se ejecute correctamente
 #inicializo valores a cada variable
-var_numero=6734
+var_numero="6734"
 var_suma=0
 #obtengo su longitud
 var_longitud=len(var_numero)
 #sumo cada digito a partir del índice de cada posición
-var_suma = var_numero [1] + var_numero [2]+ var_numero[3] + var_numero [4]
+var_suma = var_numero[0] + var_numero[1]+ var_numero[2] + var_numero[3]
+var_suma=float(var_suma)
 #utilizo una condición y el operador aritmético // para saber si el resto da 0 y ver si es par
 if var_suma // 2 == 0:
- print ("el valor de {var_suma} es impar")
+ print ("el valor de: ",var_suma,"es impar")
+else:
+    print("el valor de: ",var_suma,"es par")
