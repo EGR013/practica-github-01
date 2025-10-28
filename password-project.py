f709727a9f3b366645bc092d1371bce15a757da3
EGR013 <erik.garcia1@campus.xaloc.org>
2025-10-28 10:47:21 +0100

    Update password-project.py

--- password-project.py
+++ password-project.py
@@ -1,4 +1,5 @@
 print("Hola usuario!,es hora de crear una contraseña segura i fiable")
+errores=0
 print("INSTRUCIONES")
 print("  1.la longitud de el password tiene que ser entre 6 i 8 caracteres")
 print("  2.forçar los siguientes valores segun la possicion indicada:")
@@ -11,3 +12,12 @@ print("     posicion 6: un numero major o igual a 6 i menor o igual a 9")
 print("     posicion 7: un dels seguents simbols: &,/,#")
 print("     posicion 8: un numero menor o igual que 5")
 password=int(input("muy bien ya sabes las normas hora de introducir tu password!: "))
+if len(password) >= 6 or len(password)<=8:
+    print("esta contraseña no tiene una longitud apropiada intentalo de nuevo")
+    if password[0] >5 or password<1:
+
+    
+    if password[1].isalpha():
+else:
+    print("esta contraseña no es apropiada intentalo de nuevo")
+
