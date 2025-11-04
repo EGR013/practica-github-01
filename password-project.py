4367745102132f0807fc9a9530002587c452bc14
EGR013 <erik.garcia1@campus.xaloc.org>
2025-11-04 10:48:14 +0100

    finished i thik
    belekdkle

--- password-project.py
+++ password-project.py
@@ -20,44 +20,50 @@ print("     posicion 6: un numero major o igual a 6 i menor o igual a 9")
 print("     posicion 7: un dels seguents simbols: &,/,#")
 print("     posicion 8: un numero menor o igual que 5")
 password=(input("muy bien ya sabes las normas hora de introducir tu password!: "))
-if len(password) >= 6 and len(password)<=8:
-    print("esta contraseña no tiene una longitud apropiada intentalo de nuevo")
-    if password[0] >5 and password[0]<1:
-        print("a") , posicion1=""
+p0=float(password[0])
+p1=password[1]
+p2=password[2]
+p3=password[3]
+p4=password[4]
+p5=float(password[5])
+p6=password[6]
+p7=float(password[7])
+if (len(password)>=6) and (len(password)<=8):
+    if p0<=5 and p0>=1:
+        posicion1=("")
     else:
         errores=errores + 1
-    if password[1].isalpha() and password[1].islower():
-       print("a") , posicion2=""
+    if p1.isalpha() and p1.islower():
+       posicion2=("")
     else:
         errores=errores + 1
-    if password[2].isalpha() and password[2].isupper():
-        print("a") , posicion3=""
+    if p2.isalpha() and p2.isupper():
+        posicion3=("")
     else:
         errores=errores + 1
-    if password[3] in ("*_@"):
-        print("a") , posicion4=""
+    if p3 in ("*_@"):
+        posicion4=("")
     else:
         errores=errores + 1
-    if password[4].isalpha() and password[4].islower():
-        print("a") , posicion5=""
+    if p4.isalpha() and p4.islower():
+        posicion5=("")
     else:
         errores=errores + 1
-    if password[5]>= 6  and password[5]<=9:
-        print("a") , posicion6=""
+    if p5>= 6  and p5<=9:
+        posicion6=("")
     else:
         errores=errores + 1
-    if password[6] in ("&/#"):
-        print("a") , posicion7=""
+    if p6 in ("&/#"):
+        posicion7=("")
     else:
         errores=errores + 1
-    if password[7]<=5:
-        print("a") , posicion8=""
+    if p7<=5:
+        posicion8=("")
     else:
-        errores=errores + 1   
-else:
-    print("esta contraseña no es apropiada intentalo de nuevo")
-
-if errores==0:
-    print("este password es correcto , felicidades! esperamos que esto te ayude a tener mas seguridad digital")
-else:
-    print("este password tiene un total de: ",errores,"errores estos esrrores estan en las siguientes posiciones: ",posicion1,",",posicion2,",",posicion3,",",posicion4,",",posicion5,",",posicion6,",",posicion7,",",posicion8)
+        errores=errores + 1 
+    if errores==0:
+     print("este password es correcto , felicidades! esperamos que esto te ayude a tener mas seguridad digital")
+    else:
+     print("este password tiene un total de: ",errores,"errores estos esrrores estan en las siguientes posiciones: ",posicion1,"",posicion2,"",posicion3,"",posicion4,"",posicion5,"",posicion6,"",posicion7,"",posicion8)
+else:  
+    print("la longitud de esta contraseña no es apropiada ya que su longitud es de",len(password),"intentalo de nuevo") 
