d0121f0b8a7d6571eac1d283ed88730e02b8d165
EGR013 <erik.garcia1@campus.xaloc.org>
2026-01-30 16:01:41 +0100

    Update ejercicio DNI.py

--- ejercicio DNI.py
+++ ejercicio DNI.py
@@ -1,30 +1,31 @@
-listanumeric=["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22"]
-listaalpha=["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"]
+listaalpha= 'TRWAGMYFPDXBNJZSQVHLCKE'
 #dni=input("introduce los numeros de tu DNI son 8 digitos: ")
 lista_intentos=[]
 good=0
 choice=1
+choices=0
 dnis=[]
 correct=[]
 incorrect=[]
 while choice==1:
     dni=input("introduce los numeros de tu DNI son 8 digitos: ")
     if len(dni)==8:
-        if dni.isnumeric:
-            resto=dni%23
+        if dni.isnumeric():
+            resto=int(dni)%23
             letra=listaalpha[resto]
-            if resto not in listanumeric:
+            if resto < 23 and resto >= 0:
+                lista_intentos.append(3)
+                nif = f"{dni}-{letra}"
+                print(nif)
+                dnis.append(dni)
+                correct.append(nif)
+                choice=input("Quieres continuar? si(1) o no(2): ")
+                
+            else:
                 print("error")
                 lista_intentos.append(2)
                 dnis.append(dni)
                 incorrect.append(dni)
-                choice=input("Quieres continuar? si(1) o no(2): ")
-            else:
-                lista_intentos.append(3)
-                list(dni).append("-",letra)
-                print("dni")
-                dnis.append(dni)
-                correct.append(dni)
                 choice=input("Quieres continuar? si(1) o no(2): ")
         else:
             print("error")
@@ -39,7 +40,6 @@ while choice==1:
         incorrect.append(dni)
         choice=input("Quieres continuar? si(1) o no(2): ")
 
-choice2=input("escoje la opcion que te interesa: 1.listar DNI correctos ordenados de menor a mayor 2.Listar DNI incorrectos ordenados de menor a mayor 3.Número total de errores 4.Número total de DNIs correctos. 5.porcentaje de DNI correctos, incorrectos, errores de longitud, errores de número, no existentes.")
 correct.sort()
 incorrect.sort()
 lengthtotal=len(lista_intentos)
@@ -48,19 +48,37 @@ uno=lista_intentos.count(1)
 dos=lista_intentos.count(2)
 totalerrores=cero+uno+dos
 totalcorrectos=lista_intentos.count(3)
-porcentajeokay=lengthtotal%totalcorrectos
-porcentajenookay=lengthtotal%totalerrores
-porcentajecero=lengthtotal%cero
-porcentajeuno=lengthtotal%uno
-porcentajedos=lengthtotal%dos
-print("Lista de DNIs correctos ordenados de menor a mayor: ",correct)
-print("Lista de DNIs incorrectos ordenados de menor a mayor: ",incorrect)
-print("Número total de errores: ",totalerrores)
-print("Número total de DNIs correctos: ",totalcorrectos)
-print("porcentaje de dnis correctos: ",porcentajeokay)
-print("porcentaje de dnis incorrectos: ",porcentajenookay)
-print("porcentaje de errores de lonjitud: ",porcentajecero)
-print("porcentaje errores de numero: ",porcentajeuno)
-print("porcentaje errores de dni no existentes: ",porcentajedos)
+porcentajeokay=int(lengthtotal%totalcorrectos)
+porcentajenookay=int(lengthtotal%totalerrores)
+porcentajecero=int(lengthtotal%cero)
+porcentajeuno=int(lengthtotal%uno)
+porcentajedos=int(lengthtotal%dos)
+while choices==0:
+    choice2=int(input("escoje la opcion que te interesa: 1.listar NIF correctos ordenados de menor a mayor 2.Listar DNI incorrectos ordenados de menor a mayor 3.Número total de errores 4.Número total de DNIs correctos. 5.porcentaje de DNI correctos, incorrectos, errores de longitud, errores de número, no existentes. 6.abandonar el programa: "))
+    if choice2==1:
+        print("Lista de NIFs correctos ordenados de menor a mayor: ",correct)
+        choices=1
+    if choice2==2:
+        print("Lista de DNIs incorrectos ordenados de menor a mayor: ",incorrect)
+        choices=2
+    if choice2==3:
+        print("Número total de errores: ",totalerrores)
+        choices=3
+    if choice2==4:
+        print("Número total de DNIs correctos: ",totalcorrectos)
+        choices=4
+    if choice2==5:
+        print("porcentaje de dnis correctos: ",porcentajeokay)
+        print("porcentaje de dnis incorrectos: ",porcentajenookay)
+        print("porcentaje de errores de lonjitud: ",porcentajecero)
+        print("porcentaje errores de numero: ",porcentajeuno)
+        print("porcentaje errores de dni no existentes: ",porcentajedos)
+        choices=5
+    if choice2==6:
+        print("FIN DEL PROGRAMA")
+        choices=6
+    else:
+        print("ERROR intentalo otra vez")
+
 #list(dni).append("-",letra)
 #print("dni")
