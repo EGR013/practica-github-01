17e804720c9d5ca95ab23a28d64971ba3a705e23
EGR013 <erik.garcia1@campus.xaloc.org>
2026-02-03 10:47:38 +0100

    t

--- ejercicio DNI.py
+++ ejercicio DNI.py
@@ -19,60 +19,67 @@ while choice==1:
                 print(nif)
                 dnis.append(dni)
                 correct.append(nif)
-                choice=input("Quieres continuar? si(1) o no(2): ")
+                choice=int(input("Quieres continuar? si(1) o no(2): "))
                 
             else:
                 print("error")
                 lista_intentos.append(2)
                 dnis.append(dni)
                 incorrect.append(dni)
-                choice=input("Quieres continuar? si(1) o no(2): ")
+                choice=int(input("Quieres continuar? si(1) o no(2): "))
         else:
             print("error")
             lista_intentos.append(1)
             dnis.append(dni)
             incorrect.append(dni)
-            choice=input("Quieres continuar? si(1) o no(2): ")
+            choice=int(input("Quieres continuar? si(1) o no(2): "))
     else:
         print("error")
         lista_intentos.append(0)
         dnis.append(dni)
         incorrect.append(dni)
-        choice=input("Quieres continuar? si(1) o no(2): ")
+        choice=int(input("Quieres continuar? si(1) o no(2): "))
 
 correct.sort()
 incorrect.sort()
-lengthtotal=len(lista_intentos)
+lengthtotal=int(len(lista_intentos))
 cero=lista_intentos.count(0)
 uno=lista_intentos.count(1)
 dos=lista_intentos.count(2)
-totalerrores=cero+uno+dos
-totalcorrectos=lista_intentos.count(3)
-porcentajeokay=int(lengthtotal%totalcorrectos)
-porcentajenookay=int(lengthtotal%totalerrores)
-porcentajecero=int(lengthtotal%cero)
-porcentajeuno=int(lengthtotal%uno)
-porcentajedos=int(lengthtotal%dos)
+totalerrores=int(cero+uno+dos)
+totalcorrectos=int(lista_intentos.count(3))
+if totalcorrectos==0:
+    porcentajeokay=0
+else:
+    porcentajeokay=round((lengthtotal/totalcorrectos)*100,1)
+porcentajenookay=round((lengthtotal/totalerrores)*100,1)
+porcentajecero=round((lengthtotal/cero)*100,1)
+porcentajeuno=round((lengthtotal/uno)*100,1)
+porcentajedos=round((lengthtotal/dos)*100,1)
 while choices==0:
     choice2=int(input("escoje la opcion que te interesa: 1.listar NIF correctos ordenados de menor a mayor 2.Listar DNI incorrectos ordenados de menor a mayor 3.Número total de errores 4.Número total de DNIs correctos. 5.porcentaje de DNI correctos, incorrectos, errores de longitud, errores de número, no existentes. 6.abandonar el programa: "))
     if choice2==1:
+        correct.sort()
         print("Lista de NIFs correctos ordenados de menor a mayor: ",correct)
         choices=1
     if choice2==2:
+        incorrect.sort()
         print("Lista de DNIs incorrectos ordenados de menor a mayor: ",incorrect)
         choices=2
     if choice2==3:
+        totalerrores=int(cero+uno+dos)
         print("Número total de errores: ",totalerrores)
         choices=3
     if choice2==4:
+        totalcorrectos=int(lista_intentos.count(3))
         print("Número total de DNIs correctos: ",totalcorrectos)
         choices=4
     if choice2==5:
-        print("porcentaje de dnis correctos: ",porcentajeokay)
-        print("porcentaje de dnis incorrectos: ",porcentajenookay)
-        print("porcentaje de errores de lonjitud: ",porcentajecero)
-        print("porcentaje errores de numero: ",porcentajeuno)
-        print("porcentaje errores de dni no existentes: ",porcentajedos)
+        print("porcentaje de dnis correctos: ",porcentajeokay,"%")
+        print("porcentaje de dnis incorrectos: ",porcentajenookay,"%")
+        print("porcentaje de errores de lonjitud: ",porcentajecero,"%")
+        print("porcentaje errores de numero: ",porcentajeuno,"%")
+        print("porcentaje errores de dni no existentes: ",porcentajedos,"%")
         choices=5
     if choice2==6:
         print("FIN DEL PROGRAMA")
