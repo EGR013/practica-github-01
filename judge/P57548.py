9bdf7280885f244ad11732f9ca15158f56ef0366
EGR013 <erik.garcia1@campus.xaloc.org>
2026-02-10 10:46:01 +0100

    Create P57548.py

---
+++ judge/P57548.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+#'''valores=input()
+#lista=valores.split()
+#if len(lista)<2:
+#    valor2=input()
+#    lista.append(valor2)
+#totales=int(lista[0])+ int(lista[1])
+#print(totales)''''
+
+
+
+number=list(str(input()).split())
+if len(number)<2:
+    number=list(str(input()).split())
+
+
+numbers=int(number[0])+int(number[1])
+print(numbers)
