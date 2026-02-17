88d53b5d4681fd12946b2b3f16a41704e4227d2a
EGR013 <erik.garcia1@campus.xaloc.org>
2026-02-17 10:49:23 +0100

    zaxsaxa

--- judge/P92351.py
+++ judge/P92351.py
@@ -2,8 +2,5 @@ import math
 t=list(str(input()).split())
 a=int(t[0])
 b=int(t[1])
-if b<=0:
-    b=float(input())
-r=(math.trunc(a/b)*-b)+a
-d=a//b
-print(d,r)
+if b>=0:
+    print(a//b,a%b)
---
+++ judge/P98960.py
@@ -0,0 +1,21 @@
+
+letraog=str(input())
+letra=letraog
+m2=2
+m=1
+p=0
+m3=3
+e=2
+
+if letraog.islower:
+    m=m3
+if letra.isupper:
+    m=m2
+
+if m==3:
+    print(letraog.upper())
+if m==2:
+    print(letra.lower())
+
+
+    
