e04f250f7f00db01fe93c0a90f9e23f0592e4035
EGR013 <erik.garcia1@campus.xaloc.org>
2026-02-24 00:40:30 +0100

    Update P61634.py

--- judge/P61634.py
+++ judge/P61634.py
@@ -1,7 +1,6 @@
-year=input()
-if year[2] and year[3]==0:
-    list.pop(0)
-    if list%4==0:
+year=int(input())
+if year%100==0:
+    if (year//100)%4==0:
         print("YES")
     else:
         print("NO")
