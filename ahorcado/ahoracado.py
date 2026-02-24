fe4ef41f5e79b9036e4cc50c90ef271dca07bcc2
EGR013 <erik.garcia1@campus.xaloc.org>
2026-02-24 10:44:28 +0100

    Update ahoracado.py

--- ahorcado/ahoracado.py
+++ ahorcado/ahoracado.py
@@ -13,32 +13,51 @@ lista_baneada=[]
 life=[]
 death=[]
 letra=""
+choice_final=1
+
 while choice_final==1:
+    p=0
     choice_add=0
-    choice_final=0
     forever=1
     lista_baneada=[]
-    palabrasecreta=Lista_palabrasecreta[random.randint(0,len(palabrasecreta))]
-    choice_add=input(int("antes de empezar quieres añadir alguna palabra a la lista de palabras secretas? si(1) o no(2): "))
+    palabrasecreta=Lista_palabrasecreta[random.randint(0,len(Lista_palabrasecreta))]
+    choice_add=int(input("antes de empezar quieres añadir alguna palabra a la lista de palabras secretas? si(1) o no(2): "))
     if choice_add==1:
         Lista_palabrasecreta.append(input())
         Lista_palabrasecreta=Lista_palabrasecreta[random.randint(0,len(Lista_palabrasecreta))]
     for x in range(0,len(palabrasecreta)):
         Lista_partida.append("_")
+    choices_almost=1
+    palabrasecreta.upper()
 
-    while choice_final==0:
+    while choices_almost==1:
         c=0
         print(Lista_partida)
         letra=input("ahora es hora de intentar adivinar la palabra introduce una letra: ")
-        if letra.isalpha and not in lista_baneada:
+        letra.upper()
+        if letra.isalpha() and not letra in lista_baneada:
             for i in range(0,len(palabrasecreta)):
                 if letra==palabrasecreta[i]:
-                    palabrasecreta[i] = letra
+                    Lista_partida[i] = letra
                     c=c+1
             if c>0:
-
-            #if c==0:
-                
+                lista_baneada.append(letra)
+                print("acierto",Lista_partida)
+                c==0
+            else:
+                lista_baneada.append
+                Lista_ahorcado_form2[p]=lista_ahorcado_form1[p]
+                print("fallo",Lista_ahorcado_form2)
+                c==0
+                p=p+1
+    if p==8:
+        print("has perdido la pertida la palabra secreta era: ",palabrasecreta,"quieres iniciar otra ronda?")
+        choice_final=int(input("introduce 1 si quieres continuar,introduce 0 si quieres dejar el programa: "))
+        choices_almost=0
+    if len(Lista_partida.clear("_"))==len(palabrasecreta):
+        print("muy bien! la palbra correcta era",Lista_partida,"quieres jugar otra partida?")
+        choice_final=int(input("introduce 1 si quieres continuar,introduce 0 si quieres dejar el programa: "))
+        choices_almost=0
 
 
 
