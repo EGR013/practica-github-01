a5a439a45fa8bd4185b479e143bc60e05a357914
EGR013 <erik.garcia1@campus.xaloc.org>
2026-03-03 10:23:56 +0100

    Create file-tutorial.py

---
+++ ejercicios guiados/file-tutorial.py
@@ -0,0 +1,10 @@
+#open()
+#r: Abrir fichero para lectura. El puntero se posiciona al principio del fichero
+#r+: Abrir fichero para lectura y escritura. El puntero se posiciona al principio del fichero
+#w: Trunca a cero la longitud o crea un fichero de texto para escritura. El puntero se posiciona al principio del fichero
+#w+: Abrir fichero para lectura y escritura. Si el fichero no existe, se crea, de lo contrario se trunca. El puntero se posiciona al principio del fichero
+#a: Abrir fichero para lectura. Se creará el fichero si no exsite. El puntero se posiciona al final del fichero.
+#a+: Abrir fichero para lectura y escritura. Se creará el fichero si no exsite. El puntero se posiciona al final del fichero.
+
+text_file = open("Text.txt")
+print(text_file)
